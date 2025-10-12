e088800640d58ae3635db11601e94f97b3eb92ff
MrCzak <75632662+MrCzak@users.noreply.github.com>
2025-10-12 02:17:26 +0200

    Delete bypass.cmd

--- bypass.cmd
+++
@@ -1,2 +0,0 @@
-curl -L -o C:\Windows\Panther\autounattend.xml https://raw.githubusercontent.com/MrCzak/win-settings/refs/heads/main/autounattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\autounattend.xml /reboot
